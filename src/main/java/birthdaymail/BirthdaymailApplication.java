9ca7318f8b72beefeb9e7ebc50526bc211512e73
Chris <Chris@Chris-Desktop>
2016-08-11 12:03:40 +0200

    Initial Commit
    Project created Spring Boot 1.4.

---
+++ src/main/java/birthdaymail/BirthdaymailApplication.java
@@ -0,0 +1,12 @@
+package birthdaymail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdaymailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdaymailApplication.class, args);
+	}
+}
